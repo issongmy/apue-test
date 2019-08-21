80569344a039d91a79f940e3cd65bc69933556ce
songmingyuan <issongmy@gmail.com>
2019-08-21 20:04:56 +0800

    temp

---
+++ mychmod.c
@@ -0,0 +1,13 @@
+#include "apue.h"
+
+int
+main(void)
+{
+    struct stat statbuf;
+
+    if (stat("foo", &statbuf) < 0)
+        err_sys("stat error for foo");
+
+    if (chmod("foo", (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0)
+        err_sys("chmod error for foo");
+}
